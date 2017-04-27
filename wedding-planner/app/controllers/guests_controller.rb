da488c189e58e3eb60981bd883acc197b9462823
Liz Rodriguez <ecrodriguez@gmail.com>
2017-04-27 15:30:56 -0400

    add guest new view

--- wedding-planner/app/controllers/guests_controller.rb
+++ wedding-planner/app/controllers/guests_controller.rb
@@ -4,20 +4,18 @@ class GuestsController < ApplicationController
   end
 
   def show
-    @guests = Guest.find(params[:id])
+    @guest = Guest.find(params[:id])
   end
-
+  def new
+    @guest = Guest.new
+  end
   def create
     @guest = Guest.new(params[:name], params[:email], params[:rsvp])
 
     if @guest.save
-        redirect_to @guest, alert: "User created successfully."
+        redirect_to '/guests/show', alert: "User created successfully."
     else
-        redirect_to new_user_path, alert: "Error creating user."
+        redirect_to 'guests/new', alert: "Error creating user."
     end
-  end
-
-  def new
-    # @guest = Guest.new
   end
 end
