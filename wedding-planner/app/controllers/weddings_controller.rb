8d7bde08425bf09e938d6f0ac2092c5691845ef9
Benjamin de Jesus <jdneb@hotmail.com>
2017-04-28 11:25:35 -0400

    font styling

--- wedding-planner/app/controllers/weddings_controller.rb
+++ wedding-planner/app/controllers/weddings_controller.rb
@@ -2,23 +2,26 @@ class WeddingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    require 'uri'
-    require 'net/http'
+    # if the user has a wedding planned
+      # go to action show/wedding_id
+    # else
+      require 'uri'
+      require 'net/http'
 
-    url = URI("https://api.yelp.com/v3/businesses/search?term=hotel&location=11211&radius=10000")
+      url = URI("https://api.yelp.com/v3/businesses/search?term=hotel&location=11211&radius=10000")
 
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    request = Net::HTTP::Get.new(url)
-    request["authorization"] = 'Bearer BBr89v-kCWEA95W-USjvDWpmxT0oNUTU3WOk_AIzlzeXbXi565mLiqaViVUDxN-76M01YnskaIppjUs-ufs2oK6UGQ_aBqgCYwnL7k_3XMVAzEGWzOnQiVwvyt0AWXYx'
-    request["cache-control"] = 'no-cache'
-    request["postman-token"] = '8e478b8a-0733-29e3-7a39-05a2f5284178'
+      request = Net::HTTP::Get.new(url)
+      request["authorization"] = 'Bearer BBr89v-kCWEA95W-USjvDWpmxT0oNUTU3WOk_AIzlzeXbXi565mLiqaViVUDxN-76M01YnskaIppjUs-ufs2oK6UGQ_aBqgCYwnL7k_3XMVAzEGWzOnQiVwvyt0AWXYx'
+      request["cache-control"] = 'no-cache'
+      request["postman-token"] = '8e478b8a-0733-29e3-7a39-05a2f5284178'
 
-    response = http.request(request)
-    puts response.body
-    @body = response.body
+      response = http.request(request)
+      puts response.body
+      @body = response.body
   end
   def show
     @wedding = Wedding.find_by(id: params[:id])
