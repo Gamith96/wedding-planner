9e6c3d1760162d906e90009aae6fad3f626d7f06
Adam Roberts <aroberts85@yahoo.com>
2017-04-28 10:49:20 -0400

    add location link locationindex to locationshow

--- wedding-planner/app/controllers/locations_controller.rb
+++ wedding-planner/app/controllers/locations_controller.rb
@@ -3,7 +3,17 @@ class LocationsController < ApplicationController
     @locations = Location.all
   end
   def show
-    @location = location.find(params[:name])
-
+    puts "this is the params: #{params}"
+    @location = Location.find(params[:name])
   end
+
+  # def create
+  #   @location = Locations.new(location_params)
+  #     if @location.save
+  #       redirect_to location_index_path
+  #     else
+  #       render "show"
+  #     end
+  #   end
+
 end
