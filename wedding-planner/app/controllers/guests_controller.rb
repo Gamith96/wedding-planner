dd1bbe7fb6d8665c08a942500a524c25cd04d45b
Liz Rodriguez <ecrodriguez@gmail.com>
2017-05-02 10:32:33 -0400

    add more details to guest page

--- wedding-planner/app/controllers/guests_controller.rb
+++ wedding-planner/app/controllers/guests_controller.rb
@@ -5,6 +5,13 @@ class GuestsController < ApplicationController
 
   def show
     @guest = Guest.find(params[:id])
+    @wedding = Wedding.find_by(id: @guest.wedding_id)
+
+    @location = Location.find_by(location: params[:address])
+    @response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=" + @location + "&units=imperial&appid=629364d30da8a324a9929aa07d828b16")
+    puts @response.parsed_response
+
+
   end
   def new
     @guests = Guest.all
